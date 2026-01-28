6043e3135d97186fc15c124593b660587f82cbe4
Rakibur Rahman <rakiburrahman.code@gmail.com>
2026-01-28 22:54:23 +0530

    Sleep.js has been added

---
+++ Sleep.js
@@ -0,0 +1,30 @@
+// Question:
+
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+// Note that minor deviation from millis in the actual sleep duration is acceptable.
+
+// Example 1:
+// Input: millis = 100
+// Output: 100
+// Explanation: It should return a promise that resolves after 100ms.
+// let t = Date.now();
+// sleep(100).then(() => {
+//   console.log(Date.now() - t); // 100
+// });
+
+// Example 2:
+// Input: millis = 200
+// Output: 200
+// Explanation: It should return a promise that resolves after 200ms.
+
+// Solution:
+
+let millis = 100;
+
+async function sleep(millis) {
+  console.log("waiting....");
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+let t = Date.now();
+sleep(millis).then(() => console.log(Date.now() - t)); // 100
